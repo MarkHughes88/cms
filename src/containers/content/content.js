1cb2db896d51be2b1dfed0bc0bca1750be73f3e6
Mark Hughes <Mark.Hughes@terraquest.co.uk>
2017-11-28 11:44:32 +0000

    help page added

--- src/containers/content/content.js
+++ src/containers/content/content.js
@@ -4,15 +4,17 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './content.scss';
 
 import Home from '../../components/home/home';
+import Help from '../../components/help/help';
 
 class Content extends Component {
   render() {
     return (
-		<div className='app-container__content'>
-			<Router>
-	  		  	<Route exact path="/" component={ () => <Home /> } />
-	  	  	</Router>
-		</div>
+        <Router>
+            <div className='app-container__content'>
+                <Route exact path="/" component={ () => <Home /> } />
+	  		  	<Route exact path="/Help" component={ () => <Help /> } />
+            </div>
+        </Router>
     );
   }
 }
